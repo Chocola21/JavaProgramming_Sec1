7d7bea446d2bbf2a16280b4b8b41f96284d1b391
Chocola21 <gracepornnapas21@gmail.com>
2024-01-30 14:15:56 +0700

    JavaProject_Lab11

---
+++ JavaProject_Lab11/src/Author.java
@@ -0,0 +1,7 @@
+public interface Author {
+	public abstract String getLastName();
+	public abstract String getFiratName();
+	public abstract boolean checkEmail();
+	
+}
+
---
+++ JavaProject_Lab11/src/Shape2.java
@@ -0,0 +1,7 @@
+//interface class
+public interface Shape2 {
+	//public attribute
+	public String color = "blue";
+	//public attribute method
+	public abstract double getArea();
+}
